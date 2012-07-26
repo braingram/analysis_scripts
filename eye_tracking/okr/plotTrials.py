e779ae325cd10fb8febae7a864ed874c07162c17
Brett Graham <graham@rowland.harvard.edu>
2012-07-26 15:38:35 -0400

    updates

--- eye_tracking/okr/plotTrials.py
+++ eye_tracking/okr/plotTrials.py
@@ -1,16 +1,21 @@
 #!/usr/bin/env python
 
 from pylab import *
-import mwkDataFile
+from scipy.stats import linregress
+
+from monkeyworks.data import MWKFile
 
 timeSpan = (2091571591L, 2121555477L)
 #timeSpan = (2967819517L, 2997803799L)
 
-def read_okr_trial(timeSpan, dataFileName='okr_test_20091028_B.mwk', prePostTime=2000000):
+bestTrials = [107, 138]
+goodTrials = [93, 94, 96, 106, 107, 134, 135, 138, 139, 143]
+
+def read_okr_trial(timeSpan, dataFileName='/Users/graham/Data/20091028/okr_test_20091028_B.mwk', prePostTime=2000000):
     # get events for 1 sec prior to and 1 sec after stimulus
     timeSpan = (timeSpan[0] - prePostTime, timeSpan[1] + prePostTime)
 
-    f = mwkDataFile.MWKFile(dataFileName)
+    f = MWKFile(dataFileName)
     f.open()
 
     # data in okr_test_20091028_B.mwk suffer from the h/v swap bug
@@ -133,12 +138,125 @@ def read_okr_trial(timeSpan, dataFileName='okr_test_20091028_B.mwk', prePostTime
         velocities[1:,1] = velocities[1:,1] / dTimes
     return times, angles, velocities, stimProperties
 
+def get_trial_data(whiteList):
+    ioff()
+    f = open('trialTimes', 'r')
+    trials = []
+    for (i, l) in enumerate(f):
+        if not i in whiteList:
+            continue
+        timeSpan = (int(l.split(' ')[0][1:-2]), int(l.split(' ')[1][:-3]))
+        times, angles, velocities, stim = read_okr_trial(timeSpan)
+        trials += [{'t': times, 'h': angles[:,0], 'v': angles[:,1],
+                    'velH': velocities[:,0], 'velV': velocities[:,1],
+                    'stim': stim, 'index': i},]
+    return trials
+
+def find_falls(values):
+    i = 1
+    falls = []
+    start = None
+    # find first fall
+    while values[i] >= values[i-1]:
+        i += 1
+        if i == len(values):
+            return []
+    
+    while i < len(values):
+        if values[i] >= values[i-1]:
+            if start != None:
+                falls += [{'start': start, 'length': i - 1 - start,
+                            'amp': values[i-1] - values[start]},]
+                start = None
+            i += 1
+        else:
+            if start == None:
+                start = i - 1
+            i += 1
+    return falls
+
+def find_rises(values):
+    i = 1
+    rises = []
+    start = None
+    # find first fall
+    while values[i] <= values[i-1]:
+        i += 1
+        if i == len(values):
+            return []
+    
+    while i < len(values):
+        if values[i] <= values[i-1]:
+            if start != None:
+                rises += [{'start': start, 'length': i - 1 - start,
+                            'amp': values[i-1] - values[start]},]
+                start = None
+            i += 1
+        else:
+            if start == None:
+                start = i - 1
+            i += 1
+    return rises
+
+def measure_okr(h, t, falls, minPPoints=30, minSPoints=3, minP=0.1, figNum=None):
+    pursuitVel = []
+    saccadeVel = []
+    i = 1
+    if figNum != None:
+        figure(figNum)
+    while i < len(falls):
+        pStart = falls[i-1]['start']+falls[i-1]['length']+2
+        pEnd = falls[i]['start']
+        sStart = falls[i]['start']
+        sEnd = falls[i]['start']+falls[i]['length']
+        if pEnd - pStart < minPPoints or sEnd - sStart < minSPoints:
+            i += 1
+            continue
+        pr = linregress(t[pStart:pEnd],h[pStart:pEnd])
+        sr = linregress(t[sStart:sEnd],h[sStart:sEnd])
+        if (pr[3] <= minP and sr[3] <= minP):
+            saccadeVel += [sr[0],]
+            pursuitVel += [pr[0],]
+            if figNum != None:
+                ts = array([t[sStart],t[sEnd]])
+                ys = ts * sr[0] + sr[1]
+                plot(ts,ys,c='g',linewidth=2)
+                ts = array([t[pStart],t[pEnd]])
+                ys = ts * pr[0] + pr[1]
+                plot(ts,ys,c='r',linewidth=2)
+        i += 1
+    if figNum != None:
+        plot(t,h,c='k')
+    return pursuitVel, saccadeVel
+
+def analyze_okr(trial,figNum=None):
+    if trial['stim']['direction'] == 0:
+        # look for rises
+        moves = find_rises(trial['h'])
+    else:
+        # look for falls
+        moves = find_falls(trial['h'])
+    moves = [m for m in moves if (abs(m['amp']) > 1.0)]
+    if len(moves) == 0:
+        return "No rises/falls found"
+    pV, sV = measure_okr(trial['h'], trial['t'], moves, figNum=figNum)
+    speed = trial['stim']['speed']
+    closedLoopGain = abs(median(pV))/speed
+    openLoopGain = abs(median(sV))/speed
+    yl = ylim()
+    vlines([trial['t'][m['start']] for m in moves], yl[0], yl[1])
+    ylim(yl)
+    print "%i\t%.2f\t%.2f" % (trial['index'], closedLoopGain, openLoopGain)
+    return closedLoopGain, openLoopGain, pV, sV
+
 if __name__ == '__main__':
     ioff()
     f = open('trialTimes','r')
     imgDir = 'images/'
     i = 0
-    whiteList = [107, 138]
+    #whiteList = bestTrials
+    whiteList = goodTrials
+    #whiteList = []
     for l in f:
         # add possible whiteList
         if len(whiteList) != 0:
@@ -149,8 +267,9 @@ if __name__ == '__main__':
         #     i += 1
         #     continue
         timeSpan = (int(l.split(' ')[0][1:-2]), int(l.split(' ')[1][:-3]))
+        #print 'reading trial...'
         times, angles, velocities, stim = read_okr_trial(timeSpan)
-        print stim
+        #print stim
         # def chop(N,alpha=0.05):
         #             st = arange(0, len(t) - N, N)
         #             slopes = []
@@ -177,7 +296,7 @@ if __name__ == '__main__':
         # aesthetic adjustments
         subplots_adjust(wspace=0.34,hspace=0.41)
         # save figure
-        savefig(imgDir+str(i)+'.eps')
+        savefig(imgDir+str(i)+'.jpg', dpi=300)
         close(1)
         i += 1
         #break
